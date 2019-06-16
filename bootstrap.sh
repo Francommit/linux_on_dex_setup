6a5793ee0b68315c5ce542cc3c6277498806773f
Chris Franco <11533365+Francommit@users.noreply.github.com>
2019-06-16 13:59:25 +1000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,8 @@ sudo apt-get -y update && sudo apt-get -y upgrade
 sudo apt-get install -y xfce4 xfconf
 
 # Download Arc-Manjaro theme
-sudo wget https://www.xfce-look.org/p/1234992/startdownload?file_id=1527359525&file_name=Arc-Manjaro.zip&file_type=application/zip&file_size=250287 -O /usr/share/themes/Arc-Manjaro.zip
+wget Arc-Manjaro.zip https://www.xfce-look.org/p/1234992/startdownload?file_id=1527359525&file_name=Arc-Manjaro.zip&file_type=application/zip&file_size=250287
+# -O /usr/share/themes/Arc-Manjaro.zip
  
 # TO-DO: Download Papirus icons
 # wget https://www.xfce-look.org/p/1166289/startdownload?file_id=1560595896&file_name=papirus-icon-theme-20190615.tar.xz&file_type=application/x-xz&file_size=11454460
