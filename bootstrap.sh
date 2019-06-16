6217d76d50600959a82ab2436e09848f5e1ef9ad
Chris Franco <11533365+Francommit@users.noreply.github.com>
2019-06-16 12:16:39 +1000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 
 # Upgrade existing packages
-sudo apt-get -y update && sudo apt-get -y dist-upgrade 
+sudo apt-get -y update && sudo apt-get -y upgrade 
 
 # Install xfce4 desktop environment
 sudo apt-get install -y xfce4 xfconf
